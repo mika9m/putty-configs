1634beee8647598c92222f83c6010869f3c34a4f
Mika Marjamäki <48747358+mika9m@users.noreply.github.com>
2024-01-03 10:27:42 +0200

    Update ExportPuttySessions.ps1

--- ExportPuttySessions.ps1
+++ ExportPuttySessions.ps1
@@ -1,5 +1,5 @@
 $Date = Get-Date -Format "yyyyMMddHHmmss"
-$FileName = [Environment]::GetFolderPath("MyDocuments") + "\putty_sessions_$Date.reg"
+$FileName = [Environment]::GetFolderPath("MyDocuments") + "\putty_sessions_" + $env:computername + "_$Hostname_$Date.reg"
 $RegistryPath = "HKCU\Software\SimonTatham\PuTTY\Sessions"
 
 reg export $RegistryPath $FileName /Y
